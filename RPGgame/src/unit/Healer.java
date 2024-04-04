9d7c6dd99235ecaf914a0e5ae322dbb98edf7510
lesykim <trchol01@naver.com>
2024-04-04 16:27:03 +0900

    Add Healer

---
+++ RPGgame/src/unit/Healer.java
@@ -0,0 +1,19 @@
+package unit;
+
+public class Healer extends Player{
+
+	public Healer() {
+		super("힐러", 500);
+	}
+
+	@Override
+	public void attack(Unit unit) {
+		
+	}
+
+	@Override
+	public void Skill(Unit unit) {
+		
+	}
+
+}
