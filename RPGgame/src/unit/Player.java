6c09de916e45618e25301960658da62f1466d8e5
lesykim <trchol01@naver.com>
2024-04-04 15:57:44 +0900

    delete field

--- RPGgame/src/unit/Player.java
+++ RPGgame/src/unit/Player.java
@@ -1,40 +1,9 @@
 package unit;
 
-//import java.util.ArrayList;
-
 public abstract class Player extends Unit{
 
-	private String name;
-	private String guildName;
-	private String id;
-	private String password;
-//	ArrayList<Item> items;
-	
-	public Player(String job, int hp, String name, String id, String password) {
+	public Player(String job, int hp) {
 		super(job, hp);
-		this.name = name;
-		this.id = id;
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getGuildName() {
-		return guildName;
-	}
-	
-	public void setGildName(String guildName) {
-		this.guildName = guildName;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getPassword() {
-		return password;
 	}
 	
 }
