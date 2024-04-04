2057e74a5091cd027e5e4ea157082b4af09e09c0
lesykim <trchol01@naver.com>
2024-04-04 16:38:35 +0900

    Add ArrayList<Item> items

--- RPGgame/src/unit/Player.java
+++ RPGgame/src/unit/Player.java
@@ -1,6 +1,10 @@
 package unit;
 
+//import java.util.ArrayList;
+
 public abstract class Player extends Unit{
+	
+//	ArrayList<Item> items;
 	
 	public Player(String job, int hp) {
 		super(job, hp);
