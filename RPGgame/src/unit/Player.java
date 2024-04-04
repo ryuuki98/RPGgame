3d0175c6580ea461b63c5da0c0f4d62464b7bcd2
lesykim <trchol01@naver.com>
2024-04-04 15:49:04 +0900

    Add getter/setter

--- RPGgame/src/unit/Player.java
+++ RPGgame/src/unit/Player.java
@@ -10,8 +10,31 @@ public abstract class Player extends Unit{
 	private String password;
 //	ArrayList<Item> items;
 	
-	public Player(String job, int hp) {
+	public Player(String job, int hp, String name, String id, String password) {
 		super(job, hp);
+		this.name = name;
+		this.id = id;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getGuildName() {
+		return guildName;
+	}
+	
+	public void setGildName(String guildName) {
+		this.guildName = guildName;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
 	}
 	
 }
