ae8c7c9266f8feeec7aa2fe98315822f3a87ec8b
lesykim <trchol01@naver.com>
2024-04-05 17:57:13 +0900

    Update Item interface to class

--- RPGgame/src/item/Item.java
+++ RPGgame/src/item/Item.java
@@ -3,8 +3,24 @@ package item;
 import unit.Player;
 import unit.Unit;
 
-public interface Item {
-	public static final String name = "";
-	public static final int price = 0;
-	public void used(Unit unit);
+public abstract class Item {
+	private String name = "";
+	private int price = 0;
+	public abstract void used(Unit unit);
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
 }
