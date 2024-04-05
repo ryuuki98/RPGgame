34c0d71134583ac188b3f48c8e73e91bbad52756
lesykim <trchol01@naver.com>
2024-04-05 11:31:41 +0900

    Add CreateMonster from init()

--- RPGgame/src/stage/Normal.java
+++ RPGgame/src/stage/Normal.java
@@ -1,17 +1,35 @@
 package stage;
 
-public class Normal extends Battle{
+import java.util.ArrayList;
+import java.util.Random;
 
+import unit.Monster;
+import unit.MonsterManager;
+
+public class Normal extends Battle{
+	private Random ran = new Random();
+	private ArrayList<Monster> monsters;
+	private MonsterManager monsterManager;
+	
+	public Normal() {
+		monsters = null;
+		monsterManager = new MonsterManager();
+	}
+	
 	@Override
-	public boolean update() {
-		// TODO Auto-generated method stub
-		return false;
+	public void init() {
+		// 일반 몬스터 생성
+		monsterManager.monsters.clear();
+		monsterManager.settingMonster(ran.nextInt(3)+2);
+		monsters = null;
+		monsters = monsterManager.monsters;
+		// 여기서 길드원들 데려와야하는데....
 	}
 
 	@Override
-	public void init() {
-		// TODO Auto-generated method stub
+	public boolean update() {
 		
+		return false;
 	}
 
 }
