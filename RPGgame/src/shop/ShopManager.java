9c717ad5b200b76e6b46c5dc0ba0022f7e1d0c9e
ryuuki98 <ryuuki98@naver.com>
2024-04-08 12:11:21 +0900

    fix: 아이템 구매 기능 완성, Usermanager 인스턴스 생성 디버깅

--- RPGgame/src/shop/ShopManager.java
+++ RPGgame/src/shop/ShopManager.java
@@ -11,38 +11,39 @@ import item.Shield;
 import rpggame.Game;
 import rpggame.User;
 import rpggame.UserManager;
+import unit.Player;
 
 public class ShopManager {
 	private ArrayList<Item> itemList;
-	private UserManager userManager = UserManager.getInstance();
+	private UserManager userManager;
 	private String title = "======SHOP======";
-	
+
 	private final int BUY_ITEM = 1;
 	private final int SELL_ITEM = 2;
 	private final int END_SHOP = 0;
 	private boolean isRun = true;
-	
+
 	public ShopManager() {
 		itemList = new ArrayList<Item>();
 		itemList = createItemList();
-		
+		userManager = UserManager.getUserManager();
 	}
-	
+
 	private ArrayList<Item> createItemList() {
-		ArrayList<Item>items = new ArrayList<Item>();
-		
+		ArrayList<Item> items = new ArrayList<Item>();
+
 		BuffItem buffItem = new BuffItem();
 		items.add(buffItem);
-		
+
 		DebuffItem debuffItem = new DebuffItem();
 		items.add(debuffItem);
-		
+
 		Potion potion = new Potion();
 		items.add(potion);
-		
+
 		Shield shield = new Shield();
 		items.add(shield);
-		
+
 		return items;
 	}
 
@@ -54,20 +55,20 @@ public class ShopManager {
 				break;
 			}
 			runShopMenu(select);
-			
+
 		}
 	}
 
 	private void runShopMenu(int select) {
 		if (select == BUY_ITEM) {
 			buyItem();
-		}else if (select == SELL_ITEM) {
+		} else if (select == SELL_ITEM) {
 			sellItem();
 		}
 	}
 
 	private void sellItem() {
-		
+
 		System.out.println("아이템 팔기 메뉴");
 	}
 
@@ -75,18 +76,38 @@ public class ShopManager {
 		System.out.println(title);
 		showItemList();
 		int select = inputNumber("구매하실 물품의 번호를 입력하세요");
-		addItemToPlayer();
+		addItemToPlayer(select);
 	}
 
-	private void addItemToPlayer() {
-		User user = userManager.getUsers().get(Game.getLog());
+	private void addItemToPlayer(int select) {
+		System.out.println(userManager.getUsers());
 		
+		// 현재 로그인한 유저의 캐릭터
+		Player player = userManager.getUsers().get(Game.getLog()).getPlayer();
+
+		// 선택한 아이템
+		Item item = itemList.get(select);
+		// 아이템의 이름
+		String itemName = item.getName();
+
+		// 소지금 부족시 구매 불가
+		if (player.getMoney() - item.getPrice() < 0) {
+			System.out.println("소지하신 금액이 부족합니다.");
+			return;
+		}
+
+		// 맵에 수량 추가
+		player.getItems().put(itemName, player.getItems().get(itemName) + 1);
+		// 유저 보유 골드 차감
+		player.setMoney(player.getMoney() - item.getPrice());
+		System.out.println("구매가 완료 되었습니다.");
+
 	}
 
 	private void showItemList() {
 		for (int i = 0; i < itemList.size(); i++) {
 			Item item = itemList.get(i);
-			System.out.printf("%d) %s - %d원\n",i, item.getName(), item.getPrice());
+			System.out.printf("%d) %s - %d원\n", i, item.getName(), item.getPrice());
 		}
 	}
 
@@ -96,7 +117,6 @@ public class ShopManager {
 		System.out.println("[2.물건 판매]");
 		System.out.println("[0.종료]");
 	}
-	
 
 	private int inputNumber(String message) {
 		int number = -1;
