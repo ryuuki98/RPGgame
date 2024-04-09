023697c1873b3bb08a50d28534160c3afc43bb73
lesykim <trchol01@naver.com>
2024-04-09 14:28:25 +0900

    Add loadSystem

--- RPGgame/src/rpggame/FileManager.java
+++ RPGgame/src/rpggame/FileManager.java
@@ -1,7 +1,10 @@
 package rpggame;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +28,8 @@ import unit.Player;
 
 public class FileManager {
 	private FileWriter fileWriter;
+	private FileReader fileReader;
+	private BufferedReader bufferedReader;
 
 	private UserManager userManager;
 	private StorageManager storageManager;
@@ -119,7 +124,106 @@ public class FileManager {
 		}
 	}
 
+	private void addUser(String[] playerData, String[] userData, String[] itemData) {
+		String packageName = "unit.";
+		try {
+			Class<?> playerClass = Class.forName(packageName+playerData[0]);
+			Object obj = playerClass.getConstructor().newInstance();
+			if(obj instanceof Player) {
+				Player player = (Player) obj;
+				player.setMax_hp(Integer.parseInt(playerData[1]));
+				player.setHp(player.getMax_hp());
+				player.setPower(Integer.parseInt(playerData[2]));
+				player.setMoney(Integer.parseInt(playerData[3]));
+				player.setMax_mp(Integer.parseInt(playerData[4]));
+				player.setMp(player.getMp());
+				Map<String, Integer> map = player.getItems();
+				map.clear();
+				map.put("생명의 물약", Integer.parseInt(itemData[0]));
+				map.put("쉴드", Integer.parseInt(itemData[1]));
+				map.put("요정의 축복", Integer.parseInt(itemData[2]));
+				map.put("쇠약", Integer.parseInt(itemData[3]));
+				User user = new User(userData[0],userData[1],player);
+				users.add(user);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void parseLoadedUserData(String data) {
+		String[] lines = data.split("\n");
+		for(int i = 0; i<lines.length-1; i+=2) {
+			if(i%2==0) {
+				String[] playerData = lines[i].split("/");
+				String[] userData = lines[i+1].split("/");
+				String[] itemData = playerData[5].split(",");
+				addUser(playerData, userData, itemData);
+			}
+		}
+	}
+	
+	private void parseLoadedGuildData(String data) {
+		String[] lines = data.split("\n");
+		for(int i = 0; i<lines.length; i++) {
+			String[] guildData = lines[i].split("/");
+			storage.put(guildData[0], Integer.parseInt(guildData[2]));
+			String[] userId = guildData[1].split(",");
+			ArrayList<User> userList = new ArrayList<>();
+			for(int j = 0; j<userId.length; j++) {
+				for(User user : users) {
+					if(user.getId().equals(userId[j])) {
+						userList.add(user);
+					}
+				}
+			}
+			guildList.put(guildData[0], userList);
+		}
+	}
+	
 	public void load() {
-
+		users.clear();
+		storage.clear();
+		guildList.clear();
+		String fileName = "user.txt";
+		File file = new File(fileName);
+		if(file.exists()) {
+			String data = "";
+			try {
+				fileReader = new FileReader(file);
+				bufferedReader = new BufferedReader(fileReader);
+				
+				while(bufferedReader.ready()) {
+					data += bufferedReader.readLine()+"\n";
+				}
+				
+				parseLoadedUserData(data);
+				
+				System.out.println("파일로드 성공");
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.err.println("파일로드 실패");
+			}
+		}
+		fileName = "guild.txt";
+		file = new File(fileName);
+		if(file.exists()) {
+			String data = "";
+			try {
+				fileReader = new FileReader(file);
+				bufferedReader = new BufferedReader(fileReader);
+				
+				while(bufferedReader.ready()) {
+					data += bufferedReader.readLine()+"\n";
+				}
+				
+				parseLoadedGuildData(data);
+				
+				System.out.println("파일로드 성공");
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.err.println("파일로드 실패");
+			}
+		}
 	}
 }
